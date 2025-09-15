5e0ab1412451f91502b14d9d1c0d88dd38a999ff
nmtien11 <161571429+nmtien11@users.noreply.github.com>
2025-09-15 13:16:15 +0700

    Delete smartdoor directory

--- smartdoor/login.py
+++
@@ -1,5 +0,0 @@
-# login.py
-def login(user, password):
-    if user == "admin" and password == "1234":
-        return "Login success"
-    return "Login failed"
--- smartdoor/mobile.py
+++
@@ -1,7 +0,0 @@
-# mobile.py
-def mobile_app():
-    print(" Mobile app connected to Smart Door!")
-    print("Options: [1] Unlock door  [2] Lock door")
-
-if __name__ == "__main__":
-    mobile_app()
